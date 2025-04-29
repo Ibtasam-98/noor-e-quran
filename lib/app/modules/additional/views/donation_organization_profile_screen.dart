a8817ab846ef77f2f56c450a7741815d7ee64796
Ibtasam <quantumbytes.tech@gmail.com>
2025-04-30 04:16:17 +0700

    commit

--- lib/app/modules/additional/views/donation_organization_profile_screen.dart
+++ lib/app/modules/additional/views/donation_organization_profile_screen.dart
@@ -61,131 +61,135 @@ class OrganizationProfileScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column( // Removed Expanded here
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: AppColors.primary, width: 1),
-                  borderRadius: BorderRadius.circular(500),
-                ),
-                child: CircleAvatar(
-                  radius: 80,
-                  backgroundImage:
-                  const AssetImage("assets/images/donation_org1.png"),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: AppColors.primary, width: 1),
+                    borderRadius: BorderRadius.circular(500),
+                  ),
+                  child: CircleAvatar(
+                    radius: 80,
+                    backgroundImage:
+                    const AssetImage("assets/images/donation_org1.png"),
+                  ),
                 ),
               ),
-            ),
-            AppSizedBox.space10h,
-            CustomText(
-              title: "About Us",
-              textColor: isDarkMode ? AppColors.white : AppColors.black,
-              fontSize: 18.sp,
-              fontFamily: 'grenda',
-              maxLines: 1,
-              textAlign: TextAlign.start,
-              textOverflow: TextOverflow.ellipsis,
-            ),
-            AppSizedBox.space10h,
-            CustomText(
-              title:
-              "We are a non-profit organization dedicated to making a positive impact in our community. Our mission is to support underprivileged individuals and families by providing essential resources and services. We believe in transparency, accountability, and the power of collective action.  We strive to create sustainable solutions and empower those we serve to build better futures.",
-              textColor: isDarkMode ? AppColors.white : AppColors.black,
-              fontSize: 15.sp,
-              maxLines: 100,
-              textAlign: TextAlign.start,
-              textOverflow: TextOverflow.ellipsis,
-            ),
-            AppSizedBox.space10h,
-            CustomText(
-              title: "Donate",
-              textColor: isDarkMode ? AppColors.white : AppColors.black,
-              fontSize: 18.sp,
-              fontFamily: 'grenda',
-              maxLines: 1,
-              textAlign: TextAlign.start,
-              textOverflow: TextOverflow.ellipsis,
-            ),
-            AppSizedBox.space10h,
-            // Loop through payment details
-            ..._paymentDetails.map((details) {
-              return Container(
-                width: Get.width,
-                margin: EdgeInsets.only(bottom: 5, top: 5.h),
-                padding: EdgeInsets.all(10.h),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.r),
-                  color: AppColors.primary.withOpacity(double.parse(details["bgColor"] ?? "0.1")), // Use bgColor
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CustomText(
-                      title: details["title"] ?? "",
-                      textColor:
-                      isDarkMode ? AppColors.white : AppColors.black,
-                      fontSize: 16.sp,
-                      fontWeight: FontWeight.bold,
-                      textAlign: TextAlign.start,
-                    ),
-                    AppSizedBox.space5h,
-                    CustomText(
-                      title: details["accountName"] ?? "",
-                      textColor:
-                      isDarkMode ? AppColors.white : AppColors.black,
-                      fontSize: 14.sp,
-                      textAlign: TextAlign.start,
-                    ),
-                    AppSizedBox.space5h,
-                    CustomText(
-                      title: details["accountNumber"] ?? "",
-                      textColor:
-                      isDarkMode ? AppColors.white : AppColors.black,
-                      fontSize: 14.sp,
-                      textAlign: TextAlign.start,
-                    ),
-                    if (details["otherDetail"] != null) ...[
+              AppSizedBox.space10h,
+              CustomText(
+                title: "About Us",
+                textColor: isDarkMode ? AppColors.white : AppColors.black,
+                fontSize: 18.sp,
+                fontFamily: 'grenda',
+                maxLines: 1,
+                textAlign: TextAlign.start,
+                textOverflow: TextOverflow.ellipsis,
+              ),
+              AppSizedBox.space10h,
+              CustomText(
+                title:
+                "We are a non-profit organization dedicated to making a positive impact in our community. Our mission is to support underprivileged individuals and families by providing essential resources and services. We believe in transparency, accountability, and the power of collective action.  We strive to create sustainable solutions and empower those we serve to build better futures.",
+                textColor: isDarkMode ? AppColors.white : AppColors.black,
+                fontSize: 15.sp,
+                maxLines: 100,
+                textAlign: TextAlign.start,
+                textOverflow: TextOverflow.ellipsis,
+              ),
+              AppSizedBox.space10h,
+              CustomText(
+                title: "Donate",
+                textColor: isDarkMode ? AppColors.white : AppColors.black,
+                fontSize: 18.sp,
+                fontFamily: 'grenda',
+                maxLines: 1,
+                textAlign: TextAlign.start,
+                textOverflow: TextOverflow.ellipsis,
+              ),
+              AppSizedBox.space10h,
+              // Loop through payment details
+              ..._paymentDetails.map((details) {
+                return Container(
+                  width: Get.width,
+                  margin: EdgeInsets.only(bottom: 5, top: 5.h),
+                  padding: EdgeInsets.all(10.h),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.r),
+                    color: AppColors.primary
+                        .withOpacity(double.parse(details["bgColor"] ?? "0.1")),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CustomText(
+                        title: details["title"] ?? "",
+                        textColor:
+                        isDarkMode ? AppColors.white : AppColors.black,
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.bold,
+                        textAlign: TextAlign.start,
+                      ),
                       AppSizedBox.space5h,
                       CustomText(
-                        title: details["otherDetail"] ?? "",
+                        title: details["accountName"] ?? "",
                         textColor:
                         isDarkMode ? AppColors.white : AppColors.black,
                         fontSize: 14.sp,
                         textAlign: TextAlign.start,
                       ),
+                      AppSizedBox.space5h,
+                      CustomText(
+                        title: details["accountNumber"] ?? "",
+                        textColor:
+                        isDarkMode ? AppColors.white : AppColors.black,
+                        fontSize: 14.sp,
+                        textAlign: TextAlign.start,
+                      ),
+                      if (details["otherDetail"] != null) ...[
+                        AppSizedBox.space5h,
+                        CustomText(
+                          title: details["otherDetail"] ?? "",
+                          textColor:
+                          isDarkMode ? AppColors.white : AppColors.black,
+                          fontSize: 14.sp,
+                          textAlign: TextAlign.start,
+                        ),
+                      ],
                     ],
-                  ],
+                  ),
+                );
+              }).toList(),
+              SizedBox(height: 20.h), // Added some bottom spacing instead of Spacer
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: CustomButton(
+                  haveBgColor: true,
+                  btnTitle: "Contact Us",
+                  btnTitleColor: Colors.white,
+                  bgColor: AppColors.primary,
+                  borderRadius: 45.r,
+                  height: 45.h,
+                  useGradient: true,
+                  gradient: LinearGradient(
+                    colors: [
+                      AppColors.primary,
+                      AppColors.secondry.withOpacity(0.9),
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                  onTap: () {
+                    _launchInstagram();
+                  },
                 ),
-              );
-            }).toList(),
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-              child: CustomButton(
-                haveBgColor: true,
-                btnTitle: "Contact Us",
-                btnTitleColor: Colors.white,
-                bgColor: AppColors.primary,
-                borderRadius: 45.r,
-                height: 45.h,
-                useGradient: true,
-                gradient: LinearGradient(
-                  colors: [
-                    AppColors.primary,
-                    AppColors.secondry.withOpacity(0.9),
-                  ],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                ),
-                onTap: () {
-                  _launchInstagram();
-                },
               ),
-            ),
-          ],
+              SizedBox(height: 20.h),
+            ],
+          ),
         ),
       ),
     );
@@ -198,4 +202,3 @@ class OrganizationProfileScreen extends StatelessWidget {
     }
   }
 }
-
